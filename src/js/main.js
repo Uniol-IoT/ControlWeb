7046725fb301387c316553d4491bd032408a75dd
Moonlyy <instqntmc@gmail.com>
2022-03-24 08:57:31 +0100

    Añadido Pop-up

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 function calefacion(sala) {
   switch (sala) {
     case "comedor":
+      document.getElementById('pop-up').classList.add('visible');
       break;
     case "cocina":
       break;
     case "lavabo":
       break;
   }
+}
+
+function close() {
+  document.getElementById('pop-up').className == 'pop-up';
 }
